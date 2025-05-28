0d6d8b4d4f89be3ce52734b1ee0d326cbaf9fab2
Matthew Spencer Schechter <mschechter@midway3-login4.rcc.local>
2025-05-28 13:29:13 -0500

    MORE tests

--- test_clusterize.sh
+++ test_clusterize.sh
@@ -1,11 +1,19 @@
-echo "TEST 1"
-clusterize "echo asdf" --partition lbarreiro
+#echo "TEST 1"
+#clusterize "echo asdf" --partition lbarreiro
+#echo ""
+#
+#echo "TEST 2 - FAIL"
+#clusterize "echo asdf" --partition lbarreiro --mem-per-gpu 2G
+#echo ""
+#
+#echo "TEST 3"
+#clusterize "echo asdf" --partition lbarreiro --num-gpu 2
+#echo ""
+
+echo "TEST 4"
+clusterize "echo asdf" --partition lbarreiro-hm --mem 200G
 echo ""
 
-echo "TEST 2 - FAIL"
-clusterize "echo asdf" --partition lbarreiro --mem-per-gpu 2G
-echo ""
-
-echo "TEST 3"
-clusterize "echo asdf" --partition lbarreiro --num-gpu 2
+echo "TEST 5"
+clusterize "echo asdf" --partition lbarreiro-hm 
 echo ""
